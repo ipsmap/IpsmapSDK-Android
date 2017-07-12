320424b2c6f57cef00a589fad83307a434c1f462
chen <cyz0429@gmail.com>
2017-07-12 09:52:42 +0800

    声请位置权限

--- demo/src/main/java/com/daoyixun/ipsmap/demo/MainActivity.java
+++ demo/src/main/java/com/daoyixun/ipsmap/demo/MainActivity.java
@@ -1,20 +1,25 @@
 package com.daoyixun.ipsmap.demo;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
 import com.daoyixun.ipsmap.IpsClient;
 import com.daoyixun.ipsmap.IpsLocation;
 import com.daoyixun.ipsmap.IpsLocationListener;
 import com.daoyixun.ipsmap.IpsMapActivity;
 
-import okhttp3.OkHttpClient;
-
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_LOCATION = 1;
     private IpsClient ipsClient;
 
     @Override
@@ -33,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),ipsLocation.isInThisBuilding()+"",Toast.LENGTH_SHORT).show();
             }
         });
-        OkHttpClient okHttpClient = new OkHttpClient();
 
     }
 
@@ -44,7 +48,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startLocation(View view){
-        ipsClient.start();
+        if (hasPermission(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            //拥有了此权限
+            ipsClient.start();
+        } else {
+            //还没有对应权限
+            requestPermission(REQUEST_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    ipsClient.start();
+                } else {
+                    Toast.makeText(MainActivity.this, "请授予权限", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    public boolean hasPermission(String... permissons) {
+        for (String permisson : permissons) {
+            if ((ContextCompat.checkSelfPermission(this,
+                    permisson) != PackageManager.PERMISSION_GRANTED)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void requestPermission(int requestCode, String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, requestCode);
+        }
     }
 
     @Override
@@ -52,4 +91,5 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         ipsClient.stop();
     }
+
 }
